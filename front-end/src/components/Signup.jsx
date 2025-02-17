10c1359cf142f97dda0d43dfd711a9e235a2c8cb
Shoiab-Ahmed <shoiabahmed722@gmail.com>
2025-02-17 16:57:04 +0530

    components

---
+++ front-end/src/components/Signup.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const Signup = () => {
+    return (
+        <div>
+            <div className="flex items-center justify-center min-h-screen bg-[#EDDFB5] text-[#1D3557]">
+                <div className=''>
+
+                </div>
+            
+
+
+
+
+            </div>
+            <div>
+
+            </div>
+
+        </div>
+    )
+}
+
+export default Signup
